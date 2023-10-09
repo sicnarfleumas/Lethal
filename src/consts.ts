de44d0dc4f1728d524082362fc770e8da0a055b1
Samuel Francis <95130256+sicnarfleumas@users.noreply.github.com>
2023-10-09 21:42:55 +0530

    WIP Hero, Services

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Lethal Kickboxing Academy | Chennai';
+export const SITE_DESCRIPTION = 'Lethal Kickboxing Academy in Chennai - Where Champions Are Forged! Elevate your fitness journey with world-class training.';
