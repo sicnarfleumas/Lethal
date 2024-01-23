b0e6d3954b775c14de02e5294dfeced037dfeb45
Samuel Francis <95130256+sicnarfleumas@users.noreply.github.com>
2024-01-23 15:51:51 +0530

    GSC/GTM

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://lethalkickboxing.in/',
   integrations: [mdx(), sitemap(), react()],
   output: "server",
   adapter: netlify()
